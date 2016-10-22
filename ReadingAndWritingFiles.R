69c19416e6e4a926aa7b64854006ec63d49879a3
Andrew Perelson <Andrew Perelson>
2016-10-22 17:22:14 +0800

    v3

--- ReadingAndWritingFiles.R
+++ ReadingAndWritingFiles.R
@@ -1,3 +1,4 @@
+##CSV
 if (!file.exists("data")) { 
     dir.create("data") 
     }
@@ -9,6 +10,9 @@ cameraData <- read.csv("./data/cameras.csv")
 head(cameraData)
 
 
+##XLSX
+install.packages("xlsx")
+library("xlsx") #Might need to install Java as well
 
 fileUrl <- "http://themightyp.iapetus.feralhosting.com/MyTestData.xlsx"
 download.file(fileUrl, destfile="./data/MyTestData.xlsx", mode="wb")
@@ -20,3 +24,26 @@ colIndex <- 2
 rowIndex <- 2:4
 cameraSubsetData <- read.xlsx("./data/MyTestData.xlsx", sheetIndex = 1, colIndex = colIndex, rowIndex = rowIndex)
 cameraSubsetData
+
+
+##XML
+install.packages("XML")
+library(XML)
+
+fileUrl <- "http://www.w3schools.com/xml/simple.xml"
+doc <- xmlTreeParse(fileUrl, useInternalNodes = TRUE)
+rootNode <- xmlRoot(doc)
+xmlName(rootNode)
+names(rootNode)
+
+rootNode[[1]]
+rootNode[[1]][[1]]
+
+xpathSApply(rootNode, "//name", xmlValue)
+xpathSApply(rootNode, "//price", xmlValue)
+
+fileUrl <- "http://www.espn.com/nfl/team/_/name/bal/baltimore-ravens"
+doc <- htmlTreeParse(fileUrl, useInternalNodes = TRUE)
+scores <- xpathSApply(doc, "//li[@class='score']",xmlValue)
+teams <- xpathSApply(doc,"//li[@class='team-name']",xmlValue)
+
