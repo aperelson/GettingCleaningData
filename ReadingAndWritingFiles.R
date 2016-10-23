0a8caa91b1fd2a4cbe4f10ada0877e13a407a2ad
Andrew Perelson <Andrew Perelson>
2016-10-23 10:24:52 +0800

    v7

--- ReadingAndWritingFiles.R
+++ ReadingAndWritingFiles.R
@@ -73,6 +73,34 @@ DT[2,]
 DT[,table(y)]
 DT[,list(mean(x),sum(z))]
 
+DT[,w:=z^2]
+
 DT2 <- DT
 DT[,y:=2]
+head(DT2,3)
 
+DT[,m:={tmp <- (x+z); log2(tmp+5)}]
+DT[,a:=x>0]
+DT[,b:=mean(x+w), by=a]
+
+set.seed(123)
+DT <- data.table(x=sample(letters[1:3],1E5,TRUE))
+DT[,.N,by=x]
+
+DT <- data.table(x=rep(c("a","b","c"),each=100),y=rnorm(300))
+setkey(DT,x)
+DT['a']
+
+DT1 <- data.table(x=c('a','a','b','dt1'), y=1:4)
+DT2 <- data.table(x=c('a','b','dt2'), z=5:7)
+setkey(DT1,x)
+setkey(DT2,x)
+merge(DT1,DT2)
+
+
+big_df <- data.frame(x=rnorm(1E6), y=rnorm(1E6))
+file <- tempfile()
+write.table(big_df, file=file, row.names = FALSE, col.names = TRUE, sep="\t", quote = FALSE)
+
+temp1 <- fread(file) ## creates data.table - fast
+temp2 <- read.table(file, header = TRUE, sep="\t") ## creates data.frame - slow
